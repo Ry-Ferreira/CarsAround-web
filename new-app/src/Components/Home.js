795bc9bfe262f934d0a6cfb5498a455b76b548a4
Ryan <ryferreira1@outlook.es>
2020-11-07 16:07:22 -0300

    home-and-navbar

---
+++ new-app/src/Components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import homeBg from '../Media/homeCar.jpg';
+import './Styles/Home.css';
+
+// Imports MUI
+import Container from '@material-ui/core/Container';
+import { Box, Button, Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const Home = () => {
+    return (
+        <Container className="hero-home" disableGutters="true" maxWidth="false">
+            <img src={homeBg} className="hero-img" alt="car" />
+            <Box mx="auto" width="75%" className="hero-text">
+                <Box>
+                    <Box mb={2}>
+                        <Typography variant="h6" color="secondary" align="center">Dinos que quieres hacer con tu auto, y nosotros nos encargamos del resto</Typography>
+                    </Box>
+                    <Box mt={2}>
+                        <Typography variant="h2" color="secondary" align="center">Bienvenido a Cars Around, tu taller.</Typography>
+                    </Box>
+                    <Box align="center" mx="auto" width="25%" mt={2}>
+                        <Button variant="outlined" fullWidth color="secondary">
+                             Conócenos
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+export default Home;
---
+++ new-app/src/Components/sub-components/Navbar.js
@@ -0,0 +1,39 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+    text: {
+        color: "red"
+    }
+});
+
+const Navbar = () => {
+
+    const classes = useStyles();
+
+    return (
+        <AppBar position="fixed" color="transparent" className={classes.text}>
+            <Toolbar>
+                <Grid container direction="row" justify="space-between">
+                    <Box pl={4}>
+                        <Typography variant="h5">Cars Around</Typography>
+                    </Box>
+                    <Box pr={4} width="45%">
+                            <Grid container justify="space-around">
+                                <Button color="inherit">Services</Button>
+                                <Button color="inherit">About</Button>
+                                <Button color="inherit">Team</Button>
+                            </Grid>
+                    </Box>
+                </Grid>
+            </Toolbar>
+        </AppBar>
+    )
+};
+
+export default Navbar;
